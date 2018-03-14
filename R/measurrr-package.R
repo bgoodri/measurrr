c3595f37a74568e26a7c7073e38ffb5a188c4c09
Jake Thompson <wjakethompson@gmail.com>
2018-03-14 15:57:39 -0500

    add high level package documentation

---
+++ R/measurrr-package.R
@@ -0,0 +1,13 @@
+#' measurrr: Tidy Bayesian Eductional Measurment Modeling
+#'
+#' measurrr description
+#'
+#' @docType package
+#' @name measurrr
+NULL
+
+#' @importFrom rlang .data
+NULL
+
+# Define global variables
+if (getRversion() > "2.15.1") utils::globalVariables(c("."))
